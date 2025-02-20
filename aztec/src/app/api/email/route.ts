7ba0d712f97f75061802fc00cebf0503b1a6229f
Misael Esperanzate <59035611+Misael-E@users.noreply.github.com>
2025-02-19 18:35:15 -0700

    new email

--- aztec/src/app/api/email/route.ts
+++ aztec/src/app/api/email/route.ts
@@ -9,41 +9,44 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface InvoiceProps {
-  invoice: SingleInvoice;
-  totals: {
-    subtotal: string;
-    gst: string;
-    total: string;
-  };
+	invoice: SingleInvoice;
+	totals: {
+		subtotal: string;
+		gst: string;
+		total: string;
+	};
 }
 
 export async function POST(request: Request) {
-  const data = await request.json();
-  const invoiceId = parseInt(data.invoiceId);
+	const data = await request.json();
+	const invoiceId = parseInt(data.invoiceId);
 
-  const res: SingleInvoice = await prisma.invoice.findUnique({
-    where: { id: invoiceId },
-    include: {
-      customer: true,
-      services: true,
-    },
-  });
+	const res: SingleInvoice = await prisma.invoice.findUnique({
+		where: { id: invoiceId },
+		include: {
+			customer: true,
+			services: true,
+		},
+	});
 
-  if (!res) {
-    return notFound();
-  }
+	if (!res) {
+		return notFound();
+	}
 
-  const { subtotal, gst, total } = calculateInvoiceTotals(res.services);
-  try {
-    await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: `${res.customer.email}`,
-      subject: `You received a new invoice (#${String(res?.id).padStart(6, "0")})`,
-      react: Email({ invoice: res, totals: { subtotal, gst, total } }),
-    });
+	const { subtotal, gst, total } = calculateInvoiceTotals(res.services);
+	try {
+		await resend.emails.send({
+			from: "Aztec Auto Glass <noreply@invoice.aztecautoglass.ca>",
+			to: `${res.customer.email}`,
+			subject: `You received a new invoice (#${String(res?.id).padStart(
+				6,
+				"0"
+			)})`,
+			react: Email({ invoice: res, totals: { subtotal, gst, total } }),
+		});
 
-    return NextResponse.json({ status: "Ok" });
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
+		return NextResponse.json({ status: "Ok" });
+	} catch (error) {
+		return NextResponse.json({ error }, { status: 500 });
+	}
 }
