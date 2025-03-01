98fa8f5576c7962d6b52186e8360407fd6a54da8
Misael Esperanzate <misael.esperanzate@hotmail.com>
2025-03-01 00:41:15 -0700

    Update client.ts

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "../prisma/generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -6,7 +6,7 @@ export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export * from "../generated/client";
+export * from "../prisma/generated/client";
 
 // import { PrismaClient } from "@prisma/client";
 
